b9e8f01c069008eb45f3828d16e3b75a8a1266de
hyfgg <hyfgg20070723@qq.com>
2025-11-30 18:19:06 +0800

    Model: Add validation in setters and optimise toString()

--- src/model/Weapon.java
+++ src/model/Weapon.java
@@ -2,18 +2,67 @@ package model;
 
 //Data model class for weapon equipment, used to store core information of weapons.
 //This class stores information like ID, type, name, power, and usage of a weapon.
+//All validation logic is in Model layer to reduce coupling with Store layer.
+
 public class Weapon {
-    private String id;
-    private String type;
-    private String name;
-    private int power;
-    private String usage;
+    private String id = "";
+    private String type = "";
+    private String name = "";
+    private int power = 1;
+    private String usage = "";
+
+    //Default constructor
+    public Weapon(){}
 
     public Weapon(String id, String type, String name, int power, String usage) {
+        setId(id);
+        setType(type);
+        setName(name);
+        setPower(power);
+        setUsage(usage);
+    }
+
+    //Set weapon ID with validation
+    public void setId(String id){
+        if (id.isEmpty()){
+            System.out.println("Error: Weapon ID cannot be empty!");
+            return;
+        }
+        if(id.length()<3){
+            System.out.println("Error: Weapon ID must be at least 3 characters!");
+            return;
+        }
         this.id = id;
+    }
+
+    //Set weapon type with validation
+    public void setType(String type){
+        if(!type.equals("Sea")&&!type.equals("Air")&&!type.equals("Land")){
+            System.out.println("Error: Weapon type must be Sea/Land/Air!");
+            return;
+        }
         this.type = type;
+    }
+
+    //Set weapon name with validation: not empty.
+    public void setName(String name){
+        if(!name.isEmpty()){
+            System.out.println("Error: Weapon name cannot be empty!");
+            return;
+        }
         this.name = name;
+    }
+
+    //Set weapon power with validation
+    public void setPower(int power){
+        if(power < 1 || power > 100){
+            System.out.println("Error: Power must be between 1 and 100!");
+            return;
+        }
         this.power = power;
+    }
+
+    public void setUsage(String usage) {
         this.usage = usage;
     }
 
@@ -21,50 +70,31 @@ public class Weapon {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPower() {
         return power;
-    }
-
-    public void setPower(int power) {
-        this.power = power;
     }
 
     public String getUsage() {
         return usage;
     }
 
-    public void setUsage(String usage) {
-        this.usage = usage;
-    }
-
     //Return a formatted string that displays all details of the weapon.
+    //Now it's more easier to read!
     public String toString() {
         return "====== Weapon Details ======\n" +
-                "id: " + this.id + "\n" +
-                "type: " + this.type + "\n" +
-                "name: " + this.name + "\n" +
-                "power: " + this.power + "\n" +
-                "usage: " + this.usage + "\n" +
+                "id: " + id + "\n" +
+                "type: " + type + "\n" +
+                "name: " + name + "\n" +
+                "power: " + power + "\n" +
+                "usage: " + usage + "\n" +
                 "============================";
     }
 }
